e8822737849f31707a426d21f621c2303caa860e
danielchk <hollowdaniel1@gmail.com>
2021-08-18 07:14:37 -0500

    Engine functional

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path'); //Join directories
+
 //start
 const app = express()
 
 //config
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));   //directions views
+app.engine('.hbs', exphbs({
+    defaultLayout : 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir : path.join(app.get('views'), 'partials'),
+    extname: '.hbs',            //change extension in views
+    helpers: require('./lib/handlebars')  //usage of reuses
+}));
+
+app.set('view engine', '.hbs'); //engine handlebars working
 
 //middleware
 app.use(morgan('dev'));   //message to console to know what is going to server
+app.use(express.urlencoded({extended: false}));  //accept formulario but not everything
+app.use(express.json());  //tradoing json
 
 //variables
 
 //routes
-
+app.use(require('./routes'));
 //public
 
 //server start
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+//Responses
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+module.exports =router;
